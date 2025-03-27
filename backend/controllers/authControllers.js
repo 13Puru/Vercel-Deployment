58629996c2ff003a7e902a710176633279b90f6a
13Puru <13purab@gmail.com>
2025-03-27 18:10:40 +0100

    asdad

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -72,7 +72,7 @@ export const register = async (req, res) => {
 
     // Set JWT as a secure cookie
     res.cookie("token", token, {
-      httpOnly: true,
+      httpOnly: false,
       secure: process.env.NODE_ENV === "production", // Secure flag for production
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days expiry
@@ -285,7 +285,7 @@ export const login = async (req, res) => {
     );
 
     res.cookie("token", token, {
-      httpOnly: true,
+      httpOnly: false,
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
